412bc416ff32d02d8fa3c93f55c1a6458f736f90
itzkazuri <kadekjuli0604@protonmail.com>
2026-03-01 13:26:41 +0800

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import { Elysia } from "elysia";
 import { app } from "./api/app";
 
 app.listen(3000);
+
+void Elysia;
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
